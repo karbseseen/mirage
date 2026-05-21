0543c036d9bf5a428f625c01418158d614b04c76
Ivan <ivan.mih.02@gmail.com>
2026-05-21 18:06:38 +0300

    P2p init

--- src/main/scala/p2p/Message.scala
+++ src/main/scala/p2p/Message.scala
@@ -9,7 +9,7 @@ import scala.collection.mutable
 
 
 sealed trait Message extends Product:
-  def id: Peer.Id
+  def senderId: Peer.Id
 
 
 object Message:
@@ -17,38 +17,30 @@ object Message:
   object Ping:
     val cookie = 0x4777b31c02b707b5L
 
-  @Type(100) case class Ping(id: Peer.Id, cookie: Long) extends Message
-  @Type(101) case class Pong(id: Peer.Id, senderId: Peer.Id) extends Message
+  @Type(100) case class Ping(senderId: Peer.Id, cookie: Long) extends Message
+  @Type(101) case class Pong(senderId: Peer.Id, receiverId: Peer.Id) extends Message
 
-
-  private inline val PingWaitTime = 8000
-
-  private val pingTime = mutable.Map.empty[Peer.Id, Long]
-  private val pingTimeCleaner: Runnable = () =>
-    val minTime = System.currentTimeMillis - PingWaitTime
-    pingTime.filterInPlace { case (_, time) => time > minTime }
-  P2p.scheduler.scheduleWithFixedDelay(pingTimeCleaner, 10, 10, TimeUnit.MINUTES)
 
   private[p2p] def handle(message: Message, address: InetSocketAddress, channel: DatagramChannel): Unit =
     val now = System.currentTimeMillis
-    val foundPeer = P2p.peers.get(message.id)
+    val foundPeer = P2p.getPeer(message.senderId)
 
     val (foundLatency, foundVerified) = foundPeer.fold(0, false)(peer => (peer.latency, peer.verified))
     val (latency, verified) = message match
       case ping: Ping => (foundLatency, foundVerified && ping.cookie == Ping.cookie)
-      case pong: Pong => pingTime.remove(message.id)
+      case pong: Pong => P2p.pingTime.remove(message.senderId)
         .map(pingTime => (now - pingTime).toInt)
-        .filter(_ < PingWaitTime)
-        .fold(foundLatency, false)(waitTime => ((foundLatency + waitTime) / 3, pong.senderId == P2p.myId))    //current latency = waitTime / 2
+        .filter(_ < P2p.PingWaitTime)
+        .fold(foundLatency, false)(waitTime => ((foundLatency + waitTime) / 3, pong.receiverId == P2p.myId))    //current latency = waitTime / 2
       case _ => (foundLatency, foundVerified)
 
-    val peer = Peer(message.id, now, latency, verified, address, channel)
-    P2p.peers(message.id) = peer
+    val peer = Peer(message.senderId, now, latency, verified, address, channel)
+    P2p.updatePeer(peer)
 
     message match
 
       case ping: Ping =>
         if (ping.cookie == Ping.cookie)
-          P2p.send(Pong(P2p.myId, ping.id), peer)
+          P2p.send(Pong(P2p.myId, ping.senderId), peer)
 
       case _ => ()
--- src/main/scala/p2p/P2p.scala
+++ src/main/scala/p2p/P2p.scala
@@ -1,12 +1,12 @@
 package p2p
 
 import byte_codec.ByteCodec
-import p2p.Message.Pong
+import p2p.Message.{Ping, Pong}
 
 import java.net.*
 import java.nio.ByteBuffer
 import java.nio.channels.{DatagramChannel, SelectionKey, Selector}
-import java.util.concurrent.TimeUnit.MILLISECONDS
+import java.util.concurrent.TimeUnit.{MILLISECONDS, MINUTES}
 import java.util.concurrent.{Executors, Future, ScheduledExecutorService}
 import java.util.function.Consumer
 import scala.collection.mutable
@@ -16,23 +16,26 @@ import scala.util.{Random, Try}
 
 object P2p:
 
-  private inline val PeerLiveTime         = 10_000
-  private inline val PeerActiveTime       = 4_000
-  private inline val PeerPingPeriod       = 1_500
+  private[p2p] inline val PeerLiveTime    = 10_000
+  private[p2p] inline val PeerActiveTime  = 5_000
+  private[p2p] inline val PeerPingPeriod  = 1_500
+  private[p2p] inline val PingWaitTime    = 8_000
 
-  private inline val InterfaceUpdatePeriodSmall = 3_000
+  private inline val InterfaceUpdatePeriodSmall = 4_000
   private inline val InterfaceUpdatePeriodBig   = 20_000
 
-  private val multicastIp = Inet4Address.ofLiteral("239.227.162.194")
-  private val multicastPort = 42815
-  private var sockets: List[MulticastSocket] = Nil
-  private val buffer = ByteBuffer.allocate(2048)
+  private val multicastAddress = InetSocketAddress(Inet4Address.ofLiteral("239.227.162.194"), 42815)
 
   private val selector: Selector = Selector.open
+  private var sockets: List[MulticastSocket] = Nil
+  private val peers = mutable.Map.empty[Peer.Id, Peer]
+  private[p2p] val pingTime = mutable.Map.empty[Peer.Id, Long]
   private var hasActivePeers = false
 
-  val myId: Peer.Id = Peer.Id(Random.nextLong, Random.nextLong)
-  var peers = mutable.Map.empty[Peer.Id, Peer]
+  private val buffer = ByteBuffer.allocate(2048)
+
+  private var _myId = Peer.Id(Random.nextLong, Random.nextLong)
+  def myId: Peer.Id = _myId
   val scheduler: ScheduledExecutorService = Executors.newSingleThreadScheduledExecutor
 
 
@@ -45,14 +48,39 @@ object P2p:
       oldSockets.remove(interface) getOrElse new MulticastSocket(interface)
     oldSockets.values.foreach(_.channel.close())
 
-  private var future: Option[Future[?]] = None
-  private def onHasActivePeersChanged(): Unit =
-    future.foreach(_.cancel(false))
-    future = Some:
-      if (hasActivePeers)
-        scheduler.scheduleWithFixedDelay(interfaceUpdater, InterfaceUpdatePeriodBig, InterfaceUpdatePeriodBig, MILLISECONDS)
-      else
-        scheduler.scheduleWithFixedDelay(interfaceUpdater, 0, InterfaceUpdatePeriodSmall, MILLISECONDS)
+    val ping = ByteBuffer.wrap(ByteCodec.encode[Message](Ping(myId, Ping.cookie)))
+    sockets.foreach(_.channel.send(ping, multicastAddress))
+
+  private var interfaceUpdateTask: Future[?] =
+    scheduler.scheduleWithFixedDelay(interfaceUpdater, 0, InterfaceUpdatePeriodSmall, MILLISECONDS)
+
+  private def updateActivePeers(): Unit =
+    val hasActivePeers = peers.values.exists(_.isActive)
+    if (this.hasActivePeers != hasActivePeers)
+      this.hasActivePeers = hasActivePeers
+      interfaceUpdateTask.cancel(false)
+      interfaceUpdateTask =
+        if (hasActivePeers)
+          scheduler.scheduleWithFixedDelay(interfaceUpdater, InterfaceUpdatePeriodBig, InterfaceUpdatePeriodBig, MILLISECONDS)
+        else
+          scheduler.scheduleWithFixedDelay(interfaceUpdater, 0, InterfaceUpdatePeriodSmall, MILLISECONDS)
+
+
+  private[p2p] def getPeer(id: Peer.Id): Option[Peer] = peers.get(id)
+
+  private[p2p] def updatePeer(peer: Peer): Unit =
+    peers(peer.id) = peer
+    updateActivePeers()
+
+  private val peerKiller: Runnable = () =>
+    val now = System.currentTimeMillis
+    peers.filterInPlace: (_, peer) =>
+      val age = now - peer.lastSeen
+      if (age > PeerPingPeriod || !peer.verified) ping(peer)
+      age < PeerLiveTime
+    updateActivePeers()
+
+  scheduler.scheduleWithFixedDelay(peerKiller, PeerLiveTime, PeerLiveTime, MILLISECONDS)
 
 
   private val receiver: Runnable = () =>
@@ -64,12 +92,13 @@ object P2p:
             address <- Some(address).collect { case inet: InetSocketAddress => inet }
             message <- Try(ByteCodec.decode[Message](buffer.array))
           do
+            if (_myId == message.senderId) _myId = Peer.Id(Random.nextLong, Random.nextLong)    //Just in case
             Message.handle(message, address, channel)
         case _ => (),
       1000,
     )
 
-  scheduler.scheduleWithFixedDelay(receiver, 10, 0, MILLISECONDS)
+  scheduler.scheduleWithFixedDelay(receiver, 50, 0, MILLISECONDS)
 
 
   def send(message: Message, peer: Peer): Unit =
@@ -77,15 +106,26 @@ object P2p:
     peer.channel.send(data, peer.address)
 
   def sendToAll(data: ByteBuffer): Unit =
-    sockets.foreach(_.channel.send(data, InetSocketAddress(multicastIp, multicastPort)))
+    sockets.foreach(_.channel.send(data, multicastAddress))
+
+  private def ping(peer: Peer): Unit =
+    P2p.send(Ping(P2p.myId, Ping.cookie), peer)
+    pingTime(peer.id) = System.currentTimeMillis
+
+  private val pingTimeCleaner: Runnable = () =>
+    val minTime = System.currentTimeMillis - PingWaitTime
+    pingTime.filterInPlace { case (_, time) => time > minTime }
+
+  P2p.scheduler.scheduleWithFixedDelay(pingTimeCleaner, 5, 5, MINUTES)
+
 
   private class MulticastSocket(val interface: NetworkInterface):
     val channel: DatagramChannel = DatagramChannel
       .open(StandardProtocolFamily.INET)
-      .bind(InetSocketAddress("0.0.0.0", multicastPort))
+      .bind(InetSocketAddress("0.0.0.0", multicastAddress.getPort))
       .setOption(StandardSocketOptions.IP_MULTICAST_IF, interface)
     channel.configureBlocking(false)
-    channel.join(multicastIp, interface)
+    channel.join(multicastAddress.getAddress, interface)
     channel.register(selector, SelectionKey.OP_READ)
 
 
@@ -96,7 +136,8 @@ class Peer private[p2p] (
   val verified: Boolean,
   val address: InetSocketAddress,
   private[p2p] val channel: DatagramChannel,
-)
+):
+  def isActive: Boolean = verified && System.currentTimeMillis - lastSeen < P2p.PeerActiveTime
 
 object Peer:
   case class Id(part1: Long, part2: Long)
